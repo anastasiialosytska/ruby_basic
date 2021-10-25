344d5959d05d2c48336b643c173ba8ba41bb3a52
Anastasiia.Losytska <alosytska@gmail.com>
2021-10-25 19:07:22 +0300

    deleted extra whitespaces

--- lesson_1/ideal_weight.rb
+++ lesson_1/ideal_weight.rb
@@ -6,14 +6,10 @@ name.capitalize!
 print "#{name}, какой у вас рост? "
 
 height = gets.chomp.to_i
-
 ideal_weight = (height - 110) * 1.15
-
 
 if ideal_weight > 0
   puts "#{name}, Ваш идеальный вес #{ideal_weight.round(1)} кг"
 else
   puts "Ваш вес уже оптимальный"
 end
-
-

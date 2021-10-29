edf720c8f1f963ee87afebcf61d9228adb82679b
Anastasiia.Losytska <alosytska@gmail.com>
2021-10-29 12:16:23 +0300

    solution improvement

--- lesson_2/vowels.rb
+++ lesson_2/vowels.rb
@@ -3,9 +3,6 @@ vowels = ['а', 'у', 'о', 'и', 'э', 'ы', 'я', 'ю', 'е']
 
 vowel_index = Hash.new {0}
 
-alphabet.each do |letter|
-  if vowels.include?(letter)
-  	vowel_index[letter] = (alphabet.index(letter)) + 1
-  end
-end
+alphabet.each.with_index(1) { |element, index| vowel_index[element] = index if vowels.include?(element) }
+
 puts vowel_index
